e807d8a3de32271093e8783a63f7a048c73ecd46
Maddog4302 <48026104+Maddog4302@users.noreply.github.com>
2020-01-25 16:04:33 -0500

    Add files via upload

--- Robot.java
+++ Robot.java
@@ -1,5 +1,6 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.       
+                 */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
@@ -7,15 +8,20 @@
 
 package frc.robot;
 
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 
+import edu.wpi.first.cameraserver.CameraServer;
+import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.command.Command;
-import edu.wpi.first.wpilibj.command.Scheduler;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+import edu.wpi.first.wpilibj.GenericHID.Hand;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import frc.robot.commands.ExampleCommand;
-import frc.robot.subsystems.ExampleSubsystem;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -25,36 +31,56 @@ import frc.robot.subsystems.ExampleSubsystem;
  * project.
  */
 public class Robot extends TimedRobot {
-  public static ExampleSubsystem m_subsystem = new ExampleSubsystem();
-  public static OI m_oi;
-  Joystick xBoxJoy = new Joystick(0);
-  Joystick flightStick = new Joystick(1);
-  Joystick flightStickTwo = new Joystick(2);
-  Joystick arcadeStick = new Joystick(3);
+  private static final String kDefaultAuto = "Default";
+  private static final String kCustomAuto = "My Auto";
+  private String m_autoSelected;
+  private final SendableChooser<String> m_chooser = new SendableChooser<>();
+  WPI_TalonFX _frontLeftMotor = new WPI_TalonFX(3);
+  WPI_TalonFX _frontRightMotor = new WPI_TalonFX(1);
+  WPI_TalonFX _backLeftMotor = new WPI_TalonFX(4);
+  WPI_TalonFX _backRightMotor = new WPI_TalonFX(2);
+  //WPI_VictorSPX intake = new WPI_VictorSPX(10);
+
+  /* Construct drivetrain by providing master motor controllers */
+  DifferentialDrive _drive = new DifferentialDrive(_frontLeftMotor, _frontRightMotor);
+  
+
+  /* Joystick for control */
+Joystick _joy = new Joystick(0);
+CameraServer server;
+//Compressor c = new Compressor(0);
 
 
-  Command m_autonomousCommand;
-  SendableChooser<Command> m_chooser = new SendableChooser<>();
-  SendableChooser<Integer> controlChooser = new SendableChooser<>();
- 
+    
+    //DoubleSolenoid exampleDouble = new DoubleSolenoid(0, 1);
 
   /**
    * This function is run when the robot is first started up and should be
-   * used for any initialization code.
+   * used for any initialization code. 
    */
   @Override
   public void robotInit() {
-    m_oi = new OI();
-    m_chooser.setDefaultOption("Default Auto", new ExampleCommand());
-    // chooser.addOption("My Auto", new MyAutoCommand());
-    SmartDashboard.putData("Auto mode", m_chooser);
+    m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
+    m_chooser.addOption("My Auto", kCustomAuto);
+    SmartDashboard.putData("Auto choices", m_chooser);
+    _frontLeftMotor.configFactoryDefault();
+    _frontRightMotor.configFactoryDefault();
+    _backLeftMotor.configFactoryDefault();
+    _backRightMotor.configFactoryDefault();
+    //intake.configFactoryDefault();
+    _frontLeftMotor.setInverted(false); // <<<<<< Adjust this until robot drives forward when stick is forward
+    _frontRightMotor.setInverted(false);
+    _backRightMotor.setInverted(false);
+    //_backLeftMotor.setInverted(true);
+    //_backRightMotor.setInverted(true);
+    //intake.setInverted(false);
+    server = CameraServer.getInstance();
+    server.startAutomaticCapture();
 
-    controlChooser.setDefaultOption("xBox Joystick-Arcade Drive", 0);
-    controlChooser.addOption("Flight Joystick", 1);
-    controlChooser.addOption("Flight Joystick Two", 2);
-    controlChooser.addOption("Arcade Stick", 3);
+    _backLeftMotor.follow(_frontLeftMotor);
+    _backRightMotor.follow(_frontRightMotor);
 
-
+    
   }
 
   /**
@@ -70,45 +96,21 @@ public class Robot extends TimedRobot {
   }
 
   /**
-   * This function is called once each time the robot enters Disabled mode.
-   * You can use it to reset any subsystem information you want to clear when
-   * the robot is disabled.
-   */
-  @Override
-  public void disabledInit() {
-  }
-
-  @Override
-  public void disabledPeriodic() {
-    Scheduler.getInstance().run();
-  }
-
-  /**
    * This autonomous (along with the chooser code above) shows how to select
    * between different autonomous modes using the dashboard. The sendable
    * chooser code works with the Java SmartDashboard. If you prefer the
    * LabVIEW Dashboard, remove all of the chooser code and uncomment the
-   * getString code to get the auto name from the text box below the Gyro
+   * getString line to get the auto name from the text box below the Gyro
    *
-   * <p>You can add additional auto modes by adding additional commands to the
-   * chooser code above (like the commented example) or additional comparisons
-   * to the switch structure below with additional strings & commands.
+   * <p>You can add additional auto modes by adding additional comparisons to
+   * the switch structure below with additional strings. If using the
+   * SendableChooser make sure to add them to the chooser code above as well.
    */
   @Override
   public void autonomousInit() {
-    m_autonomousCommand = m_chooser.getSelected();
-
-    /*
-     * String autoSelected = SmartDashboard.getString("Auto Selector",
-     * "Default"); switch(autoSelected) { case "My Auto": autonomousCommand
-     * = new MyAutoCommand(); break; case "Default Auto": default:
-     * autonomousCommand = new ExampleCommand(); break; }
-     */
-
-    // schedule the autonomous command (example)
-    if (m_autonomousCommand != null) {
-      m_autonomousCommand.start();
-    }
+    m_autoSelected = m_chooser.getSelected();
+    // m_autoSelected = SmartDashboard.getString("Auto Selector", kDefaultAuto);
+    System.out.println("Auto selected: " + m_autoSelected);
   }
 
   /**
@@ -116,17 +118,14 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void autonomousPeriodic() {
-    Scheduler.getInstance().run();
-  }
-
-  @Override
-  public void teleopInit() {
-    // This makes sure that the autonomous stops running when
-    // teleop starts running. If you want the autonomous to
-    // continue until interrupted by another command, remove
-    // this line or comment it out.
-    if (m_autonomousCommand != null) {
-      m_autonomousCommand.cancel();
+    switch (m_autoSelected) {
+      case kCustomAuto:
+        // Put custom auto code here
+        break;
+      case kDefaultAuto:
+      default:
+        // Put default auto code here
+        break;
     }
   }
 
@@ -135,9 +134,42 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void teleopPeriodic() {
-    Scheduler.getInstance().run();
-    
 
+    /* Gamepad processing */
+		double forward = -0.75 * _joy.getY(Hand.kLeft);	// Sign this so forward is positive
+    double turn = +0.5 * _joy.getX(Hand.kLeft);       // Sign this so right is positive
+    
+        
+        /* Deadband - within 10% joystick, make it zero */
+		if (Math.abs(forward) < 0.10) {
+			forward = 0;
+		}
+		if (Math.abs(turn) < 0.10) {
+			turn = 0;
+    }
+
+   /*  if(_joy.getRawButton(1)){
+      exampleDouble.set(Value.kForward);
+    }
+  
+    if(_joy.getRawButton(2)){
+      exampleDouble.set(Value.kReverse);
+    } */
+    //intake.set(-_joy.getRawAxis(3)/2);        
+		/**
+		 * Print the joystick values to sign them, comment
+		 * out this line after checking the joystick directions. 
+		 */
+        System.out.println("JoyY:" + forward + "  turn:" + turn );
+        
+		/**
+		 * Drive the robot, 
+		 */
+    _drive.arcadeDrive(forward, (turn));
+    //SmartDashboard.putNumber("Intake Speed", -_joy.getRawAxis(3)/2);
+    SmartDashboard.putNumber("FWD Speed", forward);
+    SmartDashboard.putNumber("TURN Speed", turn);
+    SmartDashboard.updateValues();
 
   }
 
@@ -146,35 +178,5 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void testPeriodic() {
-    if(controlChooser.getSelected() == 0){
-      m_myRobot.arcadeDrive(-xBoxJoy.getY(Hand.kLeft), xBoxJoy.getX(Hand.kLeft));//arcade drive, leftJoystick=foward/backwards and turn
-    }
-
-    else if(controlChooser.getSelected() == 1){
-      m_myRobot.tankDrive(flightStick.getY(), flightStickTwo.getY()); // flight stick tankdrive
-    }
-    else if(controlChooser.getSelected() == 2){
-      m_myRobot.tankDrive(-xBoxJoy.getY(Hand.kLeft), xBoxJoy.getX(Hand.kRight)); // xBox Tankdrive
-    }
-    else if(controlChooser.getSelected() == 3){// Flight stick arcade drive
-      if(flightStick.getRawAxis(1) <= -.05 || flightStick.getRawAxis(1) >= .05){
-        //forward or back
-        SmartDashboard.putNumber("JoyY", flightStick.getRawAxis(1));//
-        robotd.arcadeDrive(flightStick.getRawAxis(1) * -1, 0);
-      }
-      if(flightStick.getRawAxis(0) >= .1 || flightStick.getRawAxis(0) <= -.1){
-        //turning
-        SmartDashboard.putNumber("JoyX", flightStick.getRawAxis(0));
-        robotd.arcadeDrive(0, flightStick.getRawAxis(0));
-      }
-    }
-      
-      else{
-        SmartDashboard.putString("nothing?", "yes");
-        m_leftMotor.set(0);
-        m_rightMotor.set(0);
-      }
-    }
-      //if forward both motors go
-      m_leadMotor.set(m_joystick.getY());
   }
+}
